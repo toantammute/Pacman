679103b03decb3eea1a6259852e3e8215375e4ef
toantammute <21110797@student.hcmute.edu.vn>
2023-11-26 15:17:04 +0700

    udate search

--- Pacman/search.py
+++ Pacman/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,6 +18,8 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+import heapq
+
 
 class SearchProblem:
     """
@@ -67,106 +69,125 @@ def tinyMazeSearch(problem):
     Returns a sequence of moves that solves tinyMaze.  For any other maze, the
     sequence of moves will be incorrect, so only use this for tinyMaze.
     """
-    print('########################', str(problem))
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
-
-########################################################################################################################
-
-#   With suggests in the documents and also what we studied for the algorithms of several search functions, we decided
-# to extract a main method for all other search methods. For the better reuse and understandability.
-#   Method description: With the format information of problem.getStartState() == (5,5),
-# problem.isGoalState(problem.getStartState()) == False, and problem.getSuccessors(problem.getStartState()) ==
-# [((5, 4), 'South', 1), ((4, 5), 'West', 1)]
-# path_to_goal == ['West', 'West', 'West', 'West', 'South', 'South', 'East', 'South', 'South', 'West']
-#   We push the general search as the format below and make a list to make sure each coordinate is visited or not.
-#   Make a loop with condition of if the algorithm is not empty, then keep running
-#   pop()(remove) coordinate and move to the next, and set the current_state
-#   If goal state == True for current state, run a roop in the path finding, if x[1] data not empty,  add x[1] to
-# path_to_goal.
-# Else will check whether the current_state is visited, if not, add to visited.
-# Later on, run the roop for successor states for current_state, and add each successtors for current state.
-# Last step, push the succesor paths to the algorithm.
-########################################################################################################################
-def graphFindPath(problem, algorithm):
-
-    algorithm.push([(problem.getStartState(), "", 0)])
-    visited_states = set()
-
-    while not algorithm.isEmpty():
-        path = algorithm.pop()
-        current_state = path[-1][0]
-
-        if problem.isGoalState(current_state):
-            path_to_goal = []
-            for x in path:
-                if len(x[1]) != 0:
-                    path_to_goal.append(x[1])
-            print('path_to_goal', path_to_goal)
-            return path_to_goal
-
-        else:
-            if current_state not in visited_states:
-                visited_states.add(current_state)
-
-                for successor in problem.getSuccessors(current_state):
-                    if successor[0] not in visited_states:
-                        successorPath = path[:]
-                        successorPath.append(successor)
-                        algorithm.push(successorPath)
-
-    return (problem, algorithm)
+    return [s, s, w, s, w, w, s, w]
 
 
-# As we studied the util.py, we find that several methods are related to the search algorithms.
-# We choose the Stack function which using the last-in-first-out logic for DFS.
-# Then we provide the stack algorithm and return to the graphFindPath method for finding the possible path.
 def depthFirstSearch(problem):
-    stack = util.Stack()
-    return graphFindPath(problem, stack)
+    """
+    Search the deepest nodes in the search tree first.
 
-# As we studied the util.py, we find that several methods are related to the search algorithms.
-# We choose the Queue function which using the first-in-first-out logic for DFS.
-# Then we provide the stack algorithm and return to the graphFindPath method for finding the possible path.
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+    """
+    # print "Start:", problem.getStartState()
+    # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    # print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    # print type(problem.getSuccessors(problem.getStartState())[0])
+    # print "(1,1) is Goal?:", problem.isGoalState((1,1))
+    # print "End Successors:", problem.getSuccessors((1,1))
+    # print '\n'
+    "* YOUR CODE HERE *"
+    # command:
+    #   python pacman.py -l tinyMaze -p SearchAgent --frameTime 0
+    #   python pacman.py -l mediumMaze -p SearchAgent --frameTime 0
+    #   python pacman.py -l bigMaze -z .5 -p SearchAgent --frameTime 0
+
+    Frontier = util.Stack()
+    Visited = []
+    Frontier.push((problem.getStartState(), []))
+    Visited.append(problem.getStartState())
+
+    while Frontier.isEmpty() == 0:
+        state, actions = Frontier.pop()
+
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in Visited:
+                if problem.isGoalState(n_state):
+                    # print 'Find Goal'
+                    return actions + [n_direction]
+                else:
+                    Frontier.push((n_state, actions + [n_direction]))
+                    Visited.append(n_state)
+
+    util.raiseNotDefined()
+
+
 def breadthFirstSearch(problem):
-    #util.raiseNotDefined()
-    queue = util.Queue()
+    """Search the shallowest nodes in the search tree first."""
+    # python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs --frameTime 0
+    # python pacman.py -l bigMaze -p SearchAgent -a fn=bfs -z .5 --frameTime 0
+    "* YOUR CODE HERE *"
 
-    return graphFindPath(problem, queue)
+    Frontier = util.Queue()
+    Visited = []
+    Frontier.push((problem.getStartState(), []))
+    # print 'Start',problem.getStartState()
+    # Visited.append( problem.getStartState() )
+
+    while Frontier.isEmpty() == 0:
+        state, actions = Frontier.pop()
+
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in Visited:
+                if problem.isGoalState(n_state):
+                    # print 'Find Goal'
+                    return actions + [n_direction]
+                Frontier.push((n_state, actions + [n_direction]))
+                Visited.append(n_state)
+
+    util.raiseNotDefined()
 
 
-# AS some parts of the condition of input changed from the graphFindPath, we decided to implement the function first
-# and refactor it later.
-# the difference from above is that PrioQ has a new value, count. It will count the priority and remove the least
-# prority target in the list. So, we name it as cost.
-# We add a new value that are same with path and call it as actions.
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    #util.raiseNotDefined()
+    "* YOUR CODE HERE *"
 
-    visited_states = []
-    algorithm = util.PriorityQueue()
-    algorithm.push((problem.getStartState(), []), 0)
-    cost = 0
-    while not algorithm.isEmpty():
-        path,actions = algorithm.pop()
+    # python pacman.py -l mediumMaze -p SearchAgent -a fn=ucs
+    # python pacman.py -l mediumDottedMaze -p StayEastSearchAgent
+    # python pacman.py -l mediumScaryMaze -p StayWestSearchAgent
 
-        if problem.isGoalState(path):
+    def _update(Frontier, item, priority):
+        for index, (p, c, i) in enumerate(Frontier.heap):
+            if i[0] == item[0]:
+                if p <= priority:
+                    break
+                del Frontier.heap[index]
+                Frontier.heap.append((priority, c, item))
+                heapq.heapify(Frontier.heap)
+                break
+        else:
+            Frontier.push(item, priority)
+
+    Frontier = util.PriorityQueue()
+    Visited = []
+    Frontier.push((problem.getStartState(), []), 0)
+    Visited.append(problem.getStartState())
+
+    while Frontier.isEmpty() == 0:
+        state, actions = Frontier.pop()
+
+        if problem.isGoalState(state):
             return actions
 
-        else:
-            if path not in visited_states:
-                visited_states.append(path)
+        if state not in Visited:
+            Visited.append(state)
 
-                for successor in problem.getSuccessors(path):
-                    if successor[0] not in visited_states:
-                        directions = successor[1]                   #single direction to go
-                        cost = actions + [directions]               #the path accumulated so far = previous + single
-                        algorithm.push((successor[0],cost),problem.getCostOfActions(cost))
-    return actions
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in Visited:
+                _update(Frontier, (n_state, actions + [n_direction]), problem.getCostOfActions(actions + [n_direction]))
+    util.raiseNotDefined()
 
 
 def nullHeuristic(state, problem=None):
@@ -176,30 +197,51 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
-def aStarSearch(problem, heuristic):
-    visited_states = []
-    algorithm = util.PriorityQueue()
-    algorithm.push((problem.getStartState(), []), nullHeuristic(problem.getStartState(), problem))
-    cost = 0
-    while not algorithm.isEmpty():
-        path, actions = algorithm.pop()
 
-        if problem.isGoalState(path):
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "* YOUR CODE HERE *"
+
+    # python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic --frameTime 0
+    # print heuristic(problem.getStartState(), problem) 34
+
+    def _update(Frontier, item, priority):
+        for index, (p, c, i) in enumerate(Frontier.heap):
+            if i[0] == item[0]:
+                if p <= priority:
+                    break
+                del Frontier.heap[index]
+                Frontier.heap.append((priority, c, item))
+                heapq.heapify(Frontier.heap)
+                break
+        else:
+            Frontier.push(item, priority)
+
+    Frontier = util.PriorityQueue()
+    Visited = []
+    Frontier.push((problem.getStartState(), []), heuristic(problem.getStartState(), problem))
+    Visited.append(problem.getStartState())
+
+    while Frontier.isEmpty() == 0:
+        state, actions = Frontier.pop()
+        # print state
+        if problem.isGoalState(state):
+            # print 'Find Goal'
             return actions
 
-        else:
-            if path not in visited_states:
-                visited_states.append(path)
+        if state not in Visited:
+            Visited.append(state)
 
-                for successor in problem.getSuccessors(path):
-                    if successor[0] not in visited_states:
-                        directions = successor[1]
-                        cost = actions + [directions]
-                        nullCost = problem.getCostOfActions(cost) + heuristic(successor[0], problem)
-                        algorithm.push((successor[0], cost), nullCost)
-        visited_states.append(path)
-    return actions
-    #util.raiseNotDefined()
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in Visited:
+                _update(Frontier, (n_state, actions + [n_direction]), \
+                        problem.getCostOfActions(actions + [n_direction]) + heuristic(n_state, problem))
+
+    util.raiseNotDefined()
+
+
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
